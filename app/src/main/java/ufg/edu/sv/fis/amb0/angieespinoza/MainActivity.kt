37cdcb41d84122aa25c0594ca55f8ef79be52d7f
unknown <UFG@CC6-14.ufg.local>
2024-04-02 20:02:51 -0600

    final

--- app/src/main/java/ufg/edu/sv/fis/amb0/angieespinoza/MainActivity.kt
+++ app/src/main/java/ufg/edu/sv/fis/amb0/angieespinoza/MainActivity.kt
@@ -2,10 +2,19 @@ package ufg.edu.sv.fis.amb0.angieespinoza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            replace<primer_fragmento>(R.id.fragment_container_view)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
+
     }
 }
